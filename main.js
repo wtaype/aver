994efc307d4229b2e0fe9ad11f20ec03d94daa18
wtaype <wilder.taype16@gmail.com>
2025-08-30 13:00:28 -0500

    El mejor proyecto aver

---
+++ main.js
@@ -0,0 +1,8 @@
+import $ from 'jquery';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+
+$(document).click(function(){
+
+  alert('Hola mundo')
+
+});
---
+++ vite.config.js
@@ -0,0 +1,5 @@
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  base: '/aver/', // Esta es la dirección para la versión más nueva
+})
