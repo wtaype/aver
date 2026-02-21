f44d44ddabefadb526e641b3e233dec4218f8c38
wtaype <wtaype@users.noreply.github.com>
2026-02-21 02:21:53 +0000

    deploy: 1e6fc88dfc3ed6dfa6e4fd0dafd0e185468ac1d2

---
+++ assets/footer-BNt6Nml4.js
@@ -0,0 +1,8 @@
+import{l as t,b as e,g as l,a as o,v as n,$ as a}from"./main-C-MIaxyj.js";import"./vendor-BwXq8HW-.js";function r(){const s=new Date;return`
+  <footer class="foo wb txc psa">
+    <span>Creado con <i class="fas fa-heart"></i> by <a class="ftx lkme" href="${t}" target="_blank">${e}</a></span>
+    <span>${l} - <span class="wty">${s.getFullYear()}</span></span>
+    <span class="abw"> | ${o} ${n} | actualizado:
+    <span class="wtu">${s.toLocaleString()}</span></span>
+  </footer>
+  `}a("body").append(r());a("head").append('<style>:root{--bgim:url("/aver/wpuntos.svg")}body{background: var(--bgim), var(--bg)}</style>');a(".wimenu").on("click",function(){a(".wiheader").toggleClass("movil"),a(this).find("i").toggleClass("fa-bars fa-times")});console.log(`✅ ${o} : ${n}`);export{r as footer};
